7e25324b2be08e8448f853ec6f9d31614404df19
yuta <yu.code.tech@gmail.com>
2023-12-13 20:54:44 +0900

    jsの順番を変えてコメントを追加

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,14 +1,14 @@
 new Splide(".splide", {
-  type: "loop",
-  fixedWidth: "200px",
-  gap: "20px",
-  arrows: false,
-  pagination: false,
+  type: "loop", // ループあり
+  arrows: false, // 矢印非表示
+  pagination: false, // ページネーション非表示
+  fixedWidth: "200px", // スライドのwidth200px
+  gap: "20px", // スライド間の余白20px
 
-  breakpoints: {
+  breakpoints: { // 767px以下のスタイル
     767: {
-      fixedWidth: "100px",
-      gap: "10px",
+      fixedWidth: "100px", // スライドのwidth100px
+      gap: "10px", // スライド間の余白10px
     },
   },
-}).mount(window.splide.Extensions);
+}).mount();
